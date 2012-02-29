580bc83dd1d13c2a0346a2dde94a73f652a3930c
djaschob@uw.edu <djaschob@uw.edu@c1e7b4c2-1915-b042-2e4f-787b16940cb6>
2012-02-29 00:40:04 +0000

    Initial add

---
+++ trunk/JobCenter_base/src/org/jobcenter/response/SendClientsCheckinLateNotificationResponse.java
@@ -0,0 +1,13 @@
+package org.jobcenter.response;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * The response to a submit job request
+ *
+ */
+@XmlRootElement(name = "sendClientsCheckinLateNotificationResponse")
+
+public class SendClientsCheckinLateNotificationResponse extends BaseResponse {
+
+}
