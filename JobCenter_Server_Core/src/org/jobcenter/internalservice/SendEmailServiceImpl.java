0dc07858c031e18783dd33dd7ca38c9d895b447c
djaschob@uw.edu <djaschob@uw.edu@c1e7b4c2-1915-b042-2e4f-787b16940cb6>
2012-09-09 01:58:28 +0000

    release 1.3 - Added daily email of all clients status at 6am ( configurable in the Spring config file ).

--- JobCenter_Server_Core/src/org/jobcenter/internalservice/SendEmailServiceImpl.java
+++ JobCenter_Server_Core/src/org/jobcenter/internalservice/SendEmailServiceImpl.java
@@ -18,12 +18,58 @@ public class SendEmailServiceImpl implements SendEmailService {
 
 	private static Logger log = Logger.getLogger(SendEmailServiceImpl.class);
 
+	
+	private static final String TEXT_CONTENT_MIME_TYPE = "text/plain";
+	
+	private static final String HTML_CONTENT_MIME_TYPE = "text/html";
+
 
 	/* (non-Javadoc)
 	 * @see org.jobcenter.internalservice.SendEmailService#sendEmail(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)
 	 */
+	@Override
 	public void sendEmail( String smtpEmailHost, String fromEmailAddress, String toEmailAddress, String emailSubject, String emailBody  ) throws Throwable {
 
+		sendEmailInternal( smtpEmailHost, fromEmailAddress, toEmailAddress, emailSubject, emailBody, TEXT_CONTENT_MIME_TYPE );
+	}
+	
+	
+
+
+	/* (non-Javadoc)
+	 * @see org.jobcenter.internalservice.SendEmailService#sendEmailHTML(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+	 */
+	@Override
+	public void sendEmailHTML(String smtpEmailHost, String fromEmailAddress, String toEmailAddress, String emailSubject, String emailBody) throws Throwable {
+
+		sendEmailInternal( smtpEmailHost, fromEmailAddress, toEmailAddress, emailSubject, emailBody, HTML_CONTENT_MIME_TYPE );
+		
+	}
+	
+	
+	/**
+	 * @param smtpEmailHost
+	 * @param fromEmailAddress
+	 * @param toEmailAddress
+	 * @param emailSubject
+	 * @param emailBody
+	 * @param contentType
+	 * @throws Throwable
+	 */
+	private void sendEmailInternal( String smtpEmailHost, String fromEmailAddress, String toEmailAddress, String emailSubject, String emailBody, String contentType  ) throws Throwable {
+
+		if ( log.isDebugEnabled() ) {
+			
+			log.debug( "sendEmail: contentType = " + contentType );
+			log.debug( "sendEmail: smtpEmailHost = " + smtpEmailHost );
+			log.debug( "sendEmail: fromEmailAddress = " + fromEmailAddress );
+			log.debug( "sendEmail: toEmailAddress = " + toEmailAddress );
+			log.debug( "sendEmail: emailSubject = " + emailSubject );
+			log.debug( "sendEmail: emailBody = " + emailBody );
+			
+		}
+		
+		
 		try {
 
 			// set the SMTP host property value
@@ -47,7 +93,9 @@ public class SendEmailServiceImpl implements SendEmailService {
 			// set the subject
 			message.setSubject( emailSubject );
 
-			message.setText( emailBody );
+//			message.setText( emailBody );
+			
+			message.setContent( emailBody, contentType );
 
 
 			// set the "To" address
@@ -65,6 +113,10 @@ public class SendEmailServiceImpl implements SendEmailService {
 			throw new RuntimeException( e );
 		}
 	}
+
+
+
+
 
 
 }
